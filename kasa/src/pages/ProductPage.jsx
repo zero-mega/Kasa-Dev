206d6fa18f59eef03d97832de038dffe6c2b29fb
0mega <hugoazzopardi@icloud.com>
2024-09-16 18:02:00 +0200

    fix productpage wip

--- kasa/src/pages/ProductPage.jsx
+++ kasa/src/pages/ProductPage.jsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 import properties from "../logements.json";
+import ErrorPage from "./ErrorPage";
+import { FaStar } from "react-icons/fa";
 
 function ProductPage() {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const property = properties.find((property) => property.id === id);
 
   if (!property) {
-    return <div>Logement introuvable</div>;
+    return <ErrorPage />;
   }
 
   return (
-    <div className="product-page">
-      <h1>{property.title}</h1>
+    <>
+       <div className="product-page">
       <img src={property.cover} alt={property.title} />
-      <p>{property.description}</p>
-      {/* Ajoutez d'autres informations comme l'équipement, la note, etc. */}
+      
+      <div className="product-info">
+      <div className="product-details">
+        <h1>{property.title}</h1>
+        <p>{property.location}</p>
+      </div>
+      <div className="product-author">
+        <img src={property.host.picture} alt={property.host.name} />
+        <p>{property.host.name}</p>
+      </div>
+      </div>
+
+      <div className="product-reviews">
+        <ul>
+          {property.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+        <div className="stars">
+          {[...Array(5)].map((_, i) => (
+            <FaStar key={i} className={i < property.rating ? "fas fa-star" : "far fa-star"} />
+          ))}
+        </div>
+      </div>
+
     </div>
+    </>
   );
 }
 
-
 export default ProductPage;
+
